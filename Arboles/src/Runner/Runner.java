9ea72a9a7fed61b7316c650b4b97961a5bf69aa4
Cristhian Pinzon <rdz.cristhian@gmail.com>
2016-10-05 22:06:19 -0500

    Update Runner.java

--- Arboles/src/Runner/Runner.java
+++ Arboles/src/Runner/Runner.java
@@ -15,6 +15,7 @@ public class Runner {
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Esto lo modifico tolo");
 
 		ArbolBinario arbol = new ArbolBinario();
 		GestionProducto gestion = new GestionProducto();
